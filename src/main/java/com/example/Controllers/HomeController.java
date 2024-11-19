21126b2f62e1c9ac06ac89e8c265e4943093b14f
Gavasi, Sofia (Stud. DACS) <s.gavasi@student.maastrichtuniversity.nl>
2024-11-19 11:35:08 +0100

    config thus

--- src/main/java/com/example/Controllers/HomeController.java
+++ src/main/java/com/example/Controllers/HomeController.java
@@ -1,14 +1,36 @@
 package com.example.Controllers;
 
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 public class HomeController {
     @GetMapping("/")
     public ResponseEntity get(){
         return ResponseEntity.ok("something!");
+    }
+    @GetMapping("/user")
+    public String getUser(OAuth2AuthenticationToken authentication){
+        // var groups = (List<String>)authentication.getPrincipal().getAttribute("groups");
+        // return groups.get(0);
+
+        var userAttributes = authentication.getPrincipal().getAttributes();
+
+        // StringBuilder sb = new StringBuilder();
+        // for(var entry : userAttributes.entrySet()){
+        //     var s = entry.getKey() + ": " + entry.getValue();
+        //     sb.append("\n").append(s);
+        // }
+
+        return "<pre> \n" +
+                userAttributes.entrySet().parallelStream().collect(
+                        StringBuilder::new,
+                        (s, e) -> s.append(e.getKey()).append(": ").append(e.getValue()),
+                        (a, b) -> a.append("\n").append(b)
+                ) +
+                "</pre>";
     }
 }
---
+++ src/main/java/com/example/Repositories/Config.java
@@ -0,0 +1,32 @@
+package com.example.Repositories;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Config{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(c -> c.disable())
+                .oauth2Login(withDefaults())
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers("/customers").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/events").authenticated()
+                        .anyRequest().permitAll()
+                )
+        ;
+
+        return http.build();
+    }
+
+}
