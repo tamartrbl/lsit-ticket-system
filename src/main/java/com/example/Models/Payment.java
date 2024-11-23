bd54a2f71d3e4372311f7918ccfee4101a7c8616
rabbit140 <92755487+rabbit140@users.noreply.github.com>
2024-11-23 18:35:29 +0100

    Update Payment.java
    updated fields

--- src/main/java/com/example/Models/Payment.java
+++ src/main/java/com/example/Models/Payment.java
@@ -4,21 +4,25 @@ import java.util.UUID;
 
 public class Payment {
     public UUID id; // Unique identifier for the payment
-    public UUID customerId; // Reference to the customer who made the payment
-    public UUID eventId; // Reference to the event the payment is for
-    public String status; // Status of the payment: "PENDING", "COMPLETED", "FAILED"
-    public String method; // Payment method, e.g., "iDEAL"
+    public UUID customer; // Reference to the customer who made the payment
+    public UUID event; // Reference to the event the payment is for
+    public PaymentState state; // Status of the payment: "PENDING", "COMPLETED", "FAILED"
     public double amount; // Payment amount
     public String timestamp; // Timestamp of the payment creation or update
 
     public Payment() {}
+    
+    public enum PaymentState {
+        PENDING,
+        COMPLETED,
+        FAILED;
+    }
 
-    public Payment(UUID customerId, UUID eventId, String method, double amount) {
+    public Payment(UUID customer, UUID event, String method, double amount) {
         this.id = UUID.randomUUID();
-        this.customerId = customerId;
-        this.eventId = eventId;
-        this.method = method;
+        this.customer = customer;
+        this.event = event;
         this.amount = amount;
-        this.status = "PENDING";
+        this.state = PaymentState.PENDING;
         this.timestamp = java.time.Instant.now().toString();
     }
