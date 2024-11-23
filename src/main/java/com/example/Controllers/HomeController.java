05164da0996ae19468a5bfa3327243eac24bd936
afiutowski <a.fiutowski@student.maastrichtuniversity.nl>
2024-11-23 17:10:23 +0100

    new user info view

--- src/main/java/com/example/Controllers/HomeController.java
+++ src/main/java/com/example/Controllers/HomeController.java
@@ -1,23 +1,52 @@
 package com.example.Controllers;
 
 
+import java.net.http.HttpClient;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class HomeController {
+    String userInfo;
+
     @GetMapping("/")
     public ResponseEntity get(){
         return ResponseEntity.ok("something!");
     }
+
     @GetMapping("/authorise")
-    public String getUser(OAuth2AuthenticationToken authentication){
+    public RedirectView getUser(OAuth2AuthenticationToken authentication){
         // var groups = (List<String>)authentication.getPrincipal().getAttribute("groups");
         // return groups.get(0);
 
         var userAttributes = authentication.getPrincipal().getAttributes();
+        userInfo = "<pre> \n" +
+            userAttributes.entrySet().parallelStream().collect(
+                StringBuilder::new,
+                (s, e) -> s.append(e.getKey()).append(": ").append(e.getValue()),
+                (a, b) -> a.append("\n").append(b)
+            ) +
+            "</pre>";
+
+
+        // StringBuilder sb = new StringBuilder();
+        // for(var entry : userAttributes.entrySet()){
+        //     var s = entry.getKey() + ": " + entry.getValue();
+        //     sb.append("\n").append(s);
+        // }
+        // session.setAttribute("userInfo", userInfo);
+        return new RedirectView("user");
+    }
+
+    @GetMapping("/user")
+    public String getUserInfo(){
+        // var groups = (List<String>)authentication.getPrincipal().getAttribute("groups");
+        // return groups.get(0);
 
         // StringBuilder sb = new StringBuilder();
         // for(var entry : userAttributes.entrySet()){
@@ -25,12 +54,11 @@ public class HomeController {
         //     sb.append("\n").append(s);
         // }
 
-        return "<pre> \n" +
-                userAttributes.entrySet().parallelStream().collect(
-                        StringBuilder::new,
-                        (s, e) -> s.append(e.getKey()).append(": ").append(e.getValue()),
-                        (a, b) -> a.append("\n").append(b)
-                ) +
-                "</pre>";
+        // String userInfo = (String) session.getAttribute("userInfo");
+        // if (userInfo == null) {
+        //     return "No user information found. Please authorise first.";
+        // }
+        return userInfo;
     }
+
 }
